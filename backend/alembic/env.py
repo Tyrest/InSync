72f9d7462c47f5af858f283ef09d94e92a09794b
Tyrest <terrifictyler8@gmail.com>
2026-04-09 12:49:53 -0400

    github actions stuff

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,10 +1,9 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
-
 from app.database import Base
 from app.models import app_config, download_task, oauth_state, platform_link, synced_playlist, track, user  # noqa: F401
+from sqlalchemy import engine_from_config, pool
 
 config = context.config
 if config.config_file_name is not None:
